0b6ffe2c434453401d2a47350152e886067547ff
Alejandro Pedraza <alejandro@buoyant.io>
2019-03-07 17:35:20 -0500

    Have the Webhook react to pod creation/update only
    This was already working almost out-of-the-box, just had to:
    
    - Change the webhook config so it watches pods instead of deployments
    - Grant some extra ClusterRole permissions
    - Add the piece that figures what's the OwnerReference and add the label
    for it
    
    Fixes #2342 and #1751
    
    Signed-off-by: Alejandro Pedraza <alejandro@buoyant.io>

--- pkg/inject/inject.go
+++ pkg/inject/inject.go
@@ -329,6 +329,10 @@ func (conf *ResourceConfig) parse(bytes []byte) error {
 	return nil
 }
 
+func (conf *ResourceConfig) GetOwnerReferences() []metav1.OwnerReference {
+	return conf.podMeta.OwnerReferences
+}
+
 func (conf *ResourceConfig) complete(template *v1.PodTemplateSpec) {
 	conf.podSpec = &template.Spec
 	conf.podMeta = objMeta{&template.ObjectMeta}
@@ -468,7 +472,7 @@ func (conf *ResourceConfig) injectObjectMeta(patch *Patch) {
 	}
 
 	for k, v := range conf.podLabels {
-		patch.addPodLabel(k, v)
+		patch.AddPodLabel(k, v)
 	}
 }
 
--- pkg/inject/patch_test.go
+++ pkg/inject/patch_test.go
@@ -32,7 +32,7 @@ func TestPatch(t *testing.T) {
 	actual.addContainer(sidecar)
 	actual.addInitContainerRoot()
 	actual.addInitContainer(init)
-	actual.addPodLabel(k8sPkg.ControllerNSLabel, controllerNamespace)
+	actual.AddPodLabel(k8sPkg.ControllerNSLabel, controllerNamespace)
 	actual.addPodAnnotation(k8sPkg.CreatedByAnnotation, createdBy)
 
 	expected := NewPatchDeployment()
